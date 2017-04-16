a147ca67a4a5f1ac0ecae60414c7f428f580b29c
maroofmf <maroofmf@usc.edu>
2017-04-15 22:23:33 -0700

    Added videoData class

---
+++ videoData.py
@@ -0,0 +1,24 @@
+'''
+Description: Read and index data.
+#----------------------------------------------------------------------------------------------------------------#
+Class functions:
+
+
+#----------------------------------------------------------------------------------------------------------------#
+Notes:
+
+'''
+
+class videoData:
+
+    def __init__(self):
+        self.__a = 1
+
+    def getFrame(self,frameNumber):
+        self.__a = 1
+
+    def iterator(self,startFrame=0):
+
+
+    def getBlocks
+
