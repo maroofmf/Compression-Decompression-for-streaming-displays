569792125e32efab335cc3d7024112c04f8dbb34
Aman Vora <amanvora@usc.edu>
2017-04-17 15:38:25 -0700

    Used array slicing to speed up getFrame()

--- videoData.py
+++ videoData.py
@@ -11,27 +11,24 @@ Notes:
 import numpy as np
 
 class videoData:
-
+    
     #------------------------------ Constructor ------------------------------#
     def __init__(self, FILE_NAME, HEIGHT, WIDTH, CHANNELS):
-        self.__videoFrames = np.fromfile(FILE_NAME, dtype ='uint8')
-        self.__width = WIDTH
-        self.__height = HEIGHT
-        self.__channels = CHANNELS
-        self.totalFrames = len(self.__videoFrames)/(WIDTH*HEIGHT*CHANNELS)
-        print 'videoData constructor'
+	self.__videoFrames = np.fromfile(FILE_NAME, dtype ='uint8')
+	self.__width = WIDTH
+	self.__height = HEIGHT
+	self.__channels = CHANNELS
+	self.totalFrames = len(self.__videoFrames)/(WIDTH*HEIGHT*CHANNELS)
+	
     def getFrame(self,frameNumber):
-        rgbArray = np.zeros((self.__height,self.__width,self.__channels),'uint8')
-        # i,j,k = [(frameNumber-1)*HEIGHT*WIDTH*CHANNELS + i*HEIGHT*WIDTH for i in range(3)]
-
-        for channel in range(self.__channels):
-            for row in range(self.__height):
-                for col in range(self.__width):
-                    i = channel*self.__height*self.__width+row*self.__width+col
-                    rgbArray[row][col][2-channel] = self.__videoFrames[self.__channels*(frameNumber-1)*self.__height*self.__width + i]
-                    
-                    
-        return rgbArray
+		frame = np.empty((self.__height,self.__width, self.__channels),'uint8')
+		frameOffset = frameNumber*self.__height*self.__width*self.__channels
+		for c in range(self.__channels):
+			channelOffset = self.__height*self.__width*c		
+			startIndex = frameOffset + channelOffset
+			endIndex =  startIndex + self.__height*self.__width
+			frame[:,:,2-c] = (self.__videoFrames[startIndex:endIndex]).reshape((self.__height,self.__width))		       			    
+		return frame
 
 
     def iterator(self,startFrame=0):
--- videoPlayer.py
+++ videoPlayer.py
@@ -8,33 +8,23 @@ Class functions:
 Notes:
 
 '''
-#import Tkinter as tk, threading
-#from PIL import Image, ImageTk
 import math
 from videoData import videoData
 import time
 import cv2
+import numpy as np
 
 class videoPlayer(videoData):
 	def __init__(self, FILENAME, HEIGHT, WIDTH, CHANNELS):
 		videoData.__init__(self, FILENAME, HEIGHT, WIDTH, CHANNELS)
-		#self.__fileName = FILENAME
-
 
 
 def main():
-    
-    vidPlayer = videoPlayer('oneperson_960_540.rgb',540, 960, 3)
-
-    for frame in vidPlayer.iterator():
-#            print 'hey'
-                  
-            cv2.imshow('frame',frame)
-            if cv2.waitKey(1) & 0xFF == ord('h'):
-                break
-        cv2.destroyAllWindows()
-
-
+	vidPlayer = videoPlayer('oneperson_960_540.rgb',540, 960, 3)
+	for frame in vidPlayer.iterator():
+		cv2.imshow('frame',frame)
+		if cv2.waitKey(1) & 0xFF == ord('h'):
+			break
 
 if __name__ == '__main__':
 	main()
