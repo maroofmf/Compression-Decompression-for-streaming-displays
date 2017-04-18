bb9c5d5de0cd113ac8f8176b160e9e4af9546cb9
Pranav Aggarwal <Pranav Aggarwal>
2017-04-18 11:51:22 -0700

    Block extraction

--- videoData.py
+++ videoData.py
@@ -9,38 +9,55 @@ Notes:
 
 '''
 import numpy as np
+import math
 
 class videoData:
-
+    
     #------------------------------ Constructor ------------------------------#
     def __init__(self, FILE_NAME, HEIGHT, WIDTH, CHANNELS):
-        self.__videoFrames = np.fromfile(FILE_NAME, dtype ='uint8')
-        self.__width = WIDTH
-        self.__height = HEIGHT
-        self.__channels = CHANNELS
-        self.totalFrames = len(self.__videoFrames)/(WIDTH*HEIGHT*CHANNELS)
-        print 'videoData constructor'
+	self.__videoFrames = np.fromfile(FILE_NAME, dtype ='uint8')
+	self.__width = WIDTH
+	self.__height = HEIGHT
+	self.__channels = CHANNELS
+	self.totalFrames = len(self.__videoFrames)/(WIDTH*HEIGHT*CHANNELS)
+	
     def getFrame(self,frameNumber):
-        rgbArray = np.zeros((self.__height,self.__width,self.__channels),'uint8')
-        # i,j,k = [(frameNumber-1)*HEIGHT*WIDTH*CHANNELS + i*HEIGHT*WIDTH for i in range(3)]
-
-        for channel in range(self.__channels):
-            for row in range(self.__height):
-                for col in range(self.__width):
-                    i = channel*self.__height*self.__width+row*self.__width+col
-                    rgbArray[row][col][2-channel] = self.__videoFrames[self.__channels*(frameNumber-1)*self.__height*self.__width + i]
-                    
-                    
-        return rgbArray
+		frame = np.empty((self.__height,self.__width, self.__channels),'uint8')
+		frameOffset = frameNumber*self.__height*self.__width*self.__channels
+		for c in range(self.__channels):
+			channelOffset = self.__height*self.__width*c		
+			startIndex = frameOffset + channelOffset
+			endIndex =  startIndex + self.__height*self.__width
+			frame[:,:,2-c] = np.copy((self.__videoFrames[startIndex:endIndex]).reshape((self.__height,self.__width)))
+		return frame
 
 
     def iterator(self,startFrame=0):
         for i in range(self.totalFrames):
             yield(self.getFrame(startFrame+i))
 
-    # def getNextBlock:
-
-
+    def getBlock(self, frameNumber, blockNumber, block_size):
+        block = np.empty((block_size,block_size, self.__channels),'uint8')
+        a = np.empty((block_size*block_size),'uint8')
+        frameOffset = frameNumber*self.__height*self.__width*self.__channels
+        for c in range(self.__channels):
+            channelOffset = self.__height*self.__width*c		
+            startIndex = frameOffset + channelOffset + (math.floor((blockNumber) / (self.__width/block_size)) * block_size * block_size) + ((blockNumber)%(self.__width/block_size) * block_size)
+            #a = self.__videoFrames[startIndex:block_size]
+            print startIndex
+            print block_size
+            print len(a)
+            for index in range(block_size):
+                #print (self.__width*(index+1))+startIndex
+                b = self.__videoFrames[(self.__width*(index))+startIndex:(self.__width*(index))+startIndex + block_size] 
+                #print len(b)                
+                a[index*block_size:index*block_size + block_size] = b
+                #print b
+#            endIndex =  block_size*block_size
+            
+            block[:,:,2-c] = np.copy(a.reshape((block_size,block_size)))
+            print block[:,:,2-c]
+        return block
 # class compression(videoData):
 
 #     def __init(self):
--- videoPlayer.py
+++ videoPlayer.py
@@ -8,33 +8,26 @@ Class functions:
 Notes:
 
 '''
-#import Tkinter as tk, threading
-#from PIL import Image, ImageTk
 import math
 from videoData import videoData
 import time
 import cv2
+import numpy as np
 
 class videoPlayer(videoData):
 	def __init__(self, FILENAME, HEIGHT, WIDTH, CHANNELS):
 		videoData.__init__(self, FILENAME, HEIGHT, WIDTH, CHANNELS)
-		#self.__fileName = FILENAME
-
 
 
 def main():
+	vidPlayer = videoPlayer('oneperson_960_540.rgb',540, 960, 3)
+	frame = vidPlayer.getBlock(0,20,16)     
+	#for frame in vidPlayer.iterator():
+	cv2.imshow('frame',frame)
+	if cv2.waitKey(0):  
+         print 'hi'     
+#        break
+	cv2.destroyAllWindows()
     
-    vidPlayer = videoPlayer('oneperson_960_540.rgb',540, 960, 3)
-
-    for frame in vidPlayer.iterator():
-#            print 'hey'
-                  
-            cv2.imshow('frame',frame)
-            if cv2.waitKey(1) & 0xFF == ord('h'):
-                break
-        cv2.destroyAllWindows()
-
-
-
 if __name__ == '__main__':
 	main()
