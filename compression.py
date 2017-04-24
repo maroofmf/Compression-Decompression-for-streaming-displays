e6bb4bcae14cb058bade371c5a3fd402de2f3ccc
pranavaggarwal01 <aggarwal.pranav01@gmail.com>
2017-04-23 20:00:28 -0700

    temp

--- compression.py
+++ compression.py
@@ -13,37 +13,43 @@ Class functions:
 Notes:
 
 '''
-class compression(segmentation):
+import numpy as np
+import cv2
+from videoData import videoData
+class compression():
     #------------------------------ Constructor ------------------------------#
-    def __init__(self, n1, n2):
+    def __init__(self, n1, n2, vidData):
         self.quantStepFG = n1
-		self.quantStepBG = n2
+        self.quantStepBG = n2
+        self.vidData = vidData
 
     def computeDCT(self,block3D, block_size):
 #        print block3D
+        blockType = 0 #To be computed from segmentation
+        blockDCTString = "Type " + str(blockType)
         block_dimention_updated = np.einsum('jki->ijk',block3D)
-        dct_file = open('DCT.cmp', 'a')
-        for channel in range (self.__channels):
-             block = block_dimention_updated[:][:][channel]
-             print 'Block',block
-             block_f = np.float32(block)  # float conversion/scale
-             dct_coeffs = cv2.dct(block_f)           # the dct
-             str1 = "Type "
-             for i in range(block_size):
-                 str1 = str1 + str(dct_coeffs[i][0:])
-             dct_row_str = str1.replace("\n", "")
-             
-                 
-             dct_file.write(dct_row_str)
-        dct_file.close
-         
+        for channel in range (self.vidData.getNumChannels()):
+            block = block_dimention_updated[:][:][channel]
+            block_f = np.float32(block)  # float conversion/scale
+            dct_coeffs = cv2.dct(block_f)           # the dct
+            channelDCTStr = str(dct_coeffs.reshape((1, block_size*block_size)))
+            blockDCTString += channelDCTStr
+        return blockDCTString
 
     def computeIDCT(self):
+        return
 
     def loadFromCMP(self):
+        return
         # set dctCOEFF
 
     def saveCMP(self):
         # put DCT into a file
+        dct_file = open('DCT.cmp', 'a')
+        block = self.vidData.getBlock(0,0,8)
+        blockDCTString = self.computeDCT(block,8)     
+        dct_file.write(blockDCTString)
+        dct_file.close
 
     def quantize(self):
+        return
